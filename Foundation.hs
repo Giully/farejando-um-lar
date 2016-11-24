9f245dbf40af362339cf5d2a8b1733a579e93019
giully <giulianne.saboya@gmail.com>
2016-11-24 23:58:34 +0000

    Adicionando configurações para a internacionalização no site

--- Foundation.hs
+++ Foundation.hs
@@ -44,6 +44,8 @@ Animal
 
 mkYesodData "App" $(parseRoutesFile "routes")
 
+mkMessage "App" "messages" "pt-br"
+
 type Form a = Html -> MForm Handler (FormResult a, Widget)
 
 instance Yesod App where
